651737de2b4514705983c2957665f73542235d7b
aimunNaharr <aimunnaharr@gmail.com>
2024-12-01 22:15:21 +0600

    fix: row data caching

--- src/components/modal/UpdateModal.jsx
+++ src/components/modal/UpdateModal.jsx
@@ -1,5 +1,5 @@
 import { Edit } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getRows } from "../../services/actions";
 import baseAxios from "../../services/api";
 import { Button } from "../ui/button";
@@ -22,6 +22,10 @@ export default function UpdateModal({ row, setData, tableHeaders }) {
   const rangeKeys = [rangeArr[0], rangeArr[rangeArr.length - 1]].join(":");
   const range = `Sheet1!${rangeKeys}`;
   const sheetId = localStorage.getItem("sheet-id");
+
+  useEffect(() => {
+    setUpdatedVal(row);
+  }, [openModal]);
 
   const values = Object.values(updatedVal);
   const handleOnChange = (key, value) => {
